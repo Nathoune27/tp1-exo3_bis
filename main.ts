908ff8db95769982b1130f9f993124d84bad84a6
Nathoune27 <68045427+Nathoune27@users.noreply.github.com>
2020-07-23 15:09:21 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,15 @@
+let Lum: number[] = []
 let X = 0
 let Y = 0
 basic.forever(function () {
+    Lum = [255, 170, 85, 0]
     X = randint(0, 4)
     Y = randint(0, 4)
     while (Y < 8) {
-        for (let var1 = 0; var1 <= 3; var1++) {
-            led.plotBrightness(X, Y, 255)
-            led.plotBrightness(X, Y - 1, 170)
-            led.plotBrightness(X, Y - 2, 85)
-            led.plotBrightness(X, Y - 3, 0)
-            Y += 1
-            basic.pause(200)
+        for (let I = 0; I <= 3; I++) {
+            led.plotBrightness(X, Y - I, Lum[I])
         }
+        Y += 1
+        basic.pause(200)
     }
 })
